04c953ed94b391bd133636aa0280ffa53e7f6566
Masashi Kawakami <m_kawakami@avelio.co.jp>
2022-06-09 20:53:43 +0900

    trimmed json values

---
+++ test.js
@@ -0,0 +1,9 @@
+// let text = 'My name is Whopper.\r\nIt is so nice to meet you!';
+// let newText = text.replace(/\r?\n/g, '<br>');
+// console.log(newText); // My name is Whopper.<br>It is so nice to meet you!
+
+let text = '     My name is Whopper. It is so nice to meet you!       ';
+let newText = text.trim();
+console.log(newText); // My name is Whopper. It is so nice to meet you!
+
+
